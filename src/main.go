f1187baf2b1041b868a18c714874da74bcd3bc21
milahntg <milahn@teamgeek.io>
2024-03-11 18:40:58 +0200

    Port Check

--- src/main.go
+++ src/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"lsplanner-go/controllers"
+	"os"
 
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
 
 func main() {
-	h := server.Default()
+
+	optimalPort := envPortOr("8888")
+
+	h := server.New(server.WithHostPorts(optimalPort))
 
 	h.GET("/ping", controllers.PingTest)
 	h.GET("/users", controllers.HandleGetUsers)
 
 	h.Spin()
+}
+
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
 }
